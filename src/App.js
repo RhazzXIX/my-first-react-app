4da405a2ffb3ad93a7438800590d50d6627702c7
RhazzXIX <jm21tonio@gmail.com>
2023-03-14 22:42:52 +0800

    Finish TOP React.JS State and Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponent from "./components/firstComponent";
+import { Component } from "react";
 
-function App() {
+// function formatAudience (audience) {
+//   return `${audience.type} ${audience.scope}`
+// }
+
+// const audience = {
+//   type: "Up-skilling",
+//   scope: "World!"
+// }
+
+// function FirstComponent (props) {
+//   return <h1>Hello {props.name}</h1>
+// }
+
+// const GitHub = () => (<a href='https://github.com/RhazzXIX'>GitHub Profile</a>)
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <FirstComponent name={formatAudience(audience)} />
+//       <FirstComponent name="RhazzIX" />
+//       <GitHub />
+//       <MyComponent title="React" />
+//     </div>
+//   );
+// }
+
+// function onClickBtn () {
+
+//   console.log('Button has bees clicked!')
+// }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+
+//   onClickBtn(){
+//     console.log('Button has bees clicked!')
+//   }
+
+//   render() {
+//     return (
+//     <div className='App'>
+//       <MyComponent title="React" onButtonClicked={this.onClickBtn} />
+//     </div>
+//     )
+//   }
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+
+    this.countUp = this.countUp.bind(this);
+  }
+
+  countUp() {
+    this.setState({
+      count: this.state.count + 1,
+      name: "Fifo",
+    });
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.countUp}>Click Me!</button>
+        <p>{this.state.count}</p>
+        <p>{this.state.name}</p>
+      </div>
+    );
+  }
+}
+
+// class DemoComponent extends Component {
+//   render() {
+//     const {user, scope} = this.props
+//     return(
+//       <div className="App">
+//         <h2>Hello {user} {scope}!</h2>
+//         <h3>Never ending pursue for improvement!</h3>
+//       </div>
+//     )
+//   }
+// }
+
+function DemoComponent(props) {
+  const { user, scope } = props;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        Hello {user} {scope}!
+      </h2>
+      <h3>Never ending pursue for improvement!</h3>
     </div>
   );
 }
 
+function Child(props) {
+  console.log(props)
+  return (
+    <div>
+      <h2>Hello, {props.name}</h2>
+      <h3>You are inside Child Component</h3>
+      <h3>Your user id is: {props.userId}</h3>
+    </div>
+  )
+}
+
 export default App;
+export { DemoComponent };
+
+export { Child }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,230 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { DemoComponent, Child } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// function tick () {
+//   const element = (
+//     <div className="App">
+//       <h1>Hello Up-skilling World!</h1>
+//       <p>It is {new Date().toLocaleTimeString()}</p>
+//     </div>
+//   )
+//   root.render(element)
+// }
+
+// function Clock(props) {
+//   return (
+//     <div className="App">
+//       <h1>Hello Up-skilling World!</h1>
+//       <h2> it is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+
+// class Clock extends Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {
+//       date: new Date(),
+//     }
+//   }
+
+//   tick() {
+//     this.setState({
+//         date: new Date()
+//       }
+//     )
+//   }
+
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.tick(),
+//       1000
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//        <h1>Hello Up-skilling World!</h1>
+//        <h2> it is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     )
+//   }
+
+// }
+
+// function tick() {
+//   root.render(<Clock date={new Date()} />);
+// }
+
+// setInterval(tick, 1000);
+
+// class MultiClocks extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Clock />
+//         <Clock />
+//         <Clock />
+//       </div>
+//     )
+//   }
+// }
+// const audience = {
+//   user: "Up-skilling",
+//   scope: "World"
+// }
+// const div = document.createElement('div');
+// div.textContent = 'test'
+
+// root.render(<DemoComponent {...audience}/>)
+
+// function Parent (props) {
+//   return(
+//     <div>
+//       <h2>You are inside Parent Component</h2>
+//       <Child name="User" userId="55555" />
+//     </div>
+//   )
+// }
+
+// root.render(<Parent />)
+
+// class ExampleClass extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>
+//           The name of the pokemons are {this.props.names.map(function nameIterator(item, i, arr) {
+//             if (i === (arr.length - 1)) return " and " + item;
+//             return " " + item;
+//           })}
+//         </h1>
+//       </div>
+//     );
+//   }
+// }
+
+// ExampleClass.defaultProps = {
+//   names: ["Pikachu", "Charmander", "Bulbasaur", "Squirtle"],
+// };
+
+// class MyClass extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       text: 'Hello!'
+//     }
+//   }
+
+//   updateState () {
+//     this.setState({
+//       text: 'Hi!'
+//     })
+//   }
+
+//   componentDidMount() {
+//     setTimeout(
+//       () => this.updateState(),
+//       5000
+//     )
+//   }
+
+//   render () {
+//     return (
+//       <div className="App">
+//         <h1>{this.state.text} Up-skilling World!</h1>
+//       </div>
+//     )
+//   }
+
+// }
+
+// class TestComponent extends Component {
+//   // state = {
+//   //   name: 'John',
+//   // };
+//   render () {
+//     return <h1 className="App">Hello {this.props.name}!</h1>
+//   }
+// }
+
+// const TestFunction = () => {
+//   const name = 'Rubic';
+//   console.log(`Hey ${name}!`)
+// }
+
+// root.render(<TestComponent name='Mike' />);
+// TestFunction();
+
+function showTime() {
+  const myElement = (
+    <div className="App">
+      <h1>Welcome to Up-skilling World!</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+
+  root.render(myElement);
+}
+
+// setInterval(showTime, 1000)
+
+
+class Clock extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    }
+    
+  }
+
+  tick() {
+    this.setState(
+      {
+        time: new Date().toLocaleTimeString()
+      }
+    )
+  }
+
+  componentDidMount() {
+    this.tick = this.tick.bind(this);
+    this.timer = setInterval( this.tick
+    , 1000
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    return(
+    <div className="App">
+      <h1>{this.props.greeting}</h1>
+      <h2>{this.state.time}</h2>
+    </div>
+    )
+  }
+}
+
+root.render(<Clock greeting = "Hello Up-skilling World!" />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
